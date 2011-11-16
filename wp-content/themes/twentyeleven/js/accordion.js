ef8d39e4c22941c8748f27791421846432b3a4a6
unknown <bmalley@.altarum.pri>
2011-11-16 16:54:06 -0500

    modified accordion.js for showcase

--- wp-content/themes/twentyeleven/js/accordion.js
+++ wp-content/themes/twentyeleven/js/accordion.js
@@ -3,6 +3,8 @@ var $j = jQuery.noConflict();
 $j(document).ready(function(){
 	$j('.service_content_container').hide();
 	//$j('.handle').filter(':first').addClass('active').next().show();
+	$j('.showcase-slide').hide();
+	$j('.showcase-slide').filter(':first').show();
 
 	$j('.handle').click(function(){
 		if( $j(this).next().is(':hidden') ) { 
@@ -10,5 +12,29 @@ $j(document).ready(function(){
 			$j(this).toggleClass('active').next().slideDown(); 
 		}
 		return false;
+	});
+	
+	$j('#showcase-next').click(function(){
+		var current = $j('.showcase-slide').filter(':visible');
+		var next;
+
+		if(current.next().hasClass('showcase-slide')) {	
+			next = current.next();
+		} else {
+			next = $j('.showcase-slide').filter(':first');
+		}
+		$j(current).add(next).toggle();
+	});
+	
+	$j('#showcase-previous').click(function(){
+		var current = $j('.showcase-slide').filter(':visible');
+		var next;
+
+		if(current.prev().hasClass('showcase-slide')) {	
+			next = current.prev();
+		} else {
+			next = $j('.showcase-slide').filter(':last');
+		}
+		$j(current).add(next).toggle();
 	});
 });
