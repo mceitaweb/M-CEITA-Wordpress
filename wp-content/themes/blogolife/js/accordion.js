126cdb92402a1264bfdcbf2c8e8c794bb7fec4f8
Benjamin Malley <benjamin.malley@gmail.com>
2011-12-02 15:40:19 -0500

    fixed references to .service_content_container

--- wp-content/themes/blogolife/js/accordion.js
+++ wp-content/themes/blogolife/js/accordion.js
@@ -1,14 +1,14 @@
 var $j = jQuery.noConflict();
 
 $j(document).ready(function(){
-	$j('.content_container').hide();
+	$j('.service_content_container').hide();
 	//$j('.handle').filter(':first').addClass('active').next().show();
 
 	$j('.handle').click(function(){
 		if( $j(this).next().is(':hidden') ) { 
 			//$j('.handle').removeClass('active').next().slideUp(); //Remove all .handle classes and slide up the immediate next container
 			$j(this).toggleClass('active').next().slideDown(); 
-		} else if( $j(this).next().is('.content_container') ) {
+		} else if( $j(this).next().is('.service_content_container') ) {
 			$j(this).removeClass('active').next().slideUp();
 		}
 		return false;
