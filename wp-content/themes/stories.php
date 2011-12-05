50fea61f9d3c941b1a27f71590ef0beb8c4853b4
Benjamin Malley <benjamin.malley@gmail.com>
2011-12-05 16:23:23 -0500

    modified stories.php

--- wp-content/themes/stories.php
+++ wp-content/themes/stories.php
@@ -1,8 +1,12 @@
 <?php
-/*
-Template Name: Stories from the Field
+/**
+ * The main template file.
+ *
+ * @package WPLOOK
+ * @subpackage BlogoLife
+ * @since BlogoLife 1.0
 */
-
 get_header();
 get_template_part('inc', 'indexloop' ) ;
+get_sidebar();
 get_footer(); ?>
