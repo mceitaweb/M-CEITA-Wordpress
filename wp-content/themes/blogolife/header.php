879c06df772d29f5b1ec1d12d5fb0a50f9c68044
Benjamin Malley <benjamin.malley@gmail.com>
2012-02-17 15:30:59 -0500

    hardcoded header image

--- wp-content/themes/blogolife/header.php
+++ wp-content/themes/blogolife/header.php
@@ -100,29 +100,7 @@ foreach ($options as $value) {
 </nav>
 <?php } ?>
 <div id="header-image">	
-<?php
-				//Code from 2011 theme by wordpress theme team
-				//Check to see if the header image has been removed
-				$header_image = get_header_image();
-				if ( ! empty( $header_image ) ) :	?>
-<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				<?php
-					// The header image
-					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
-					if ( is_singular() &&
-							has_post_thumbnail( $post->ID ) &&
-							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
-							$image[1] >= HEADER_IMAGE_WIDTH ) :
-						// Houston, we have a new header image!
-						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else : ?>
-<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-				<?php endif; // end check for featured image or standard header ?>
-			</a>
-			<?php endif; // end check for removed header image ?>
-			<?php // Has the text been hidden?
-				if ( 'blank' == get_header_textcolor() ) :
-			?>
-<?php endif; ?>
+<img src="http://204.106.31.83/wp-content/uploads/2012/02/MCEITA-logo.jpg" />
+
 </div>
 <div id="main">
