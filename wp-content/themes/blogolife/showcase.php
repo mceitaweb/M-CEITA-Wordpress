473a2227d8a95c5e89289098dfb0191b80adbe1a
Benjamin Malley <benjamin.malley@gmail.com>
2012-02-17 13:08:14 -0500

    added showcase css

--- wp-content/themes/blogolife/showcase.php
+++ wp-content/themes/blogolife/showcase.php
@@ -5,3 +5,11 @@ Template Name: Showcase
 get_header();
 get_template_part('content', 'single' ) ;
 get_footer(); ?>
+<style type='text/css'>
+.col2 {
+	width: 923px;
+}
+#content {
+	width: 972px;
+}
+</style>
