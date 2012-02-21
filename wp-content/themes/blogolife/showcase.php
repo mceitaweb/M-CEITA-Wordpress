9ff8039b075b4029f09bc1d68fb7626bb13cf5bb
Benjamin Malley <benjamin.malley@gmail.com>
2012-02-21 13:26:13 -0500

    removed entry-meta div from css

--- wp-content/themes/blogolife/showcase.php
+++ wp-content/themes/blogolife/showcase.php
@@ -7,9 +7,12 @@ get_template_part('content', 'single' ) ;
 get_footer(); ?>
 <style type='text/css'>
 .col2 {
-	width: 923px;
+	width: 925px;
 }
 .right-sidebar #content {
 	width: 972px;
+}
+.entry-meta {
+	visibility: hidden;
 }
 </style>
