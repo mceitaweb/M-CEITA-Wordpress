af1f766ef8ef57c1bc0323466c0ff63efcace3dd
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 17:22:06 -0400

    fixed apostrophes and other fixes

--- wp-content/themes/blogolife/showcasecontent.php
+++ wp-content/themes/blogolife/showcasecontent.php
@@ -6,8 +6,8 @@
  * @since BlogoLife 1.0
  */
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<header class="entry-header"><h1 class="entry-title"></h1></header>
+<article>
+<header class="entry-header"></header>
 
 	<div class="entry-content">
 
@@ -16,7 +16,7 @@
 <div class="slide-wrapper slide-height">
 <div class="slide">
 <div class="slide-content"><b>Westside PO: Edwin DeLange, DO</b>
-Dr. DeLange is a solo practitioner, utilizing Athena Health’s complete product (EPM, EHR, billing company component) for the last year. He attended the Athena user group meeting in Boston in April and is feeling very confident post meeting that he is on the right track for both improving the quality of care for his patients and achieving efficiency in his practice. Dr. DeLange has completed a Current State Assessment as well as a Meaningful Use Audit and Gap Analysis and has only has two measures to work on — the patient summary document and the security evaluation. M-CEITA is providing guidance and recommendations in order for his practice to achieve these measures.</div>
+Dr. DeLange is a solo practitioner, utilizing Athena Health's complete product (EPM, EHR, billing company component) for the last year. He attended the Athena user group meeting in Boston in April and is feeling very confident post meeting that he is on the right track for both improving the quality of care for his patients and achieving efficiency in his practice. Dr. DeLange has completed a Current State Assessment as well as a Meaningful Use Audit and Gap Analysis and has only has two measures to work on — the patient summary document and the security evaluation. M-CEITA is providing guidance and recommendations in order for his practice to achieve these measures.</div>
 </div>
 </div>
 <div class="slide-wrapper slide-height">
@@ -28,19 +28,19 @@ A rural pediatric practice was struggling with the Medicaid registration process
 <div class="slide-wrapper slide-height">
 <div class="slide">
 <div class="slide-content"><img src="http://204.106.31.83/wp-content/uploads/2012/02/sheridan_community_hospital-150x150.jpg" alt="" title="sheridan_community_hospital" width="150" height="150" class="alignleft size-thumbnail wp-image-536" /><b>Sheridan Community Hospital</b>
-Although Sheridan Community Hospital’s ambulatory practices had originally decided not to pursue the EHR incentive program–more technology seemed too big of a hurdle–Sheridan’s leaders heard an M-CEITA presentation about how to pursue Meaningful Use through the modular approach. Now Sheridan Community Hospital’s ambulatory practices are partnering with M-CEITA to qualify and attest for the Meaningful Use program.</div>
+Although Sheridan Community Hospital's ambulatory practices had originally decided not to pursue the EHR incentive program–more technology seemed too big of a hurdle–Sheridan's leaders heard an M-CEITA presentation about how to pursue Meaningful Use through the modular approach. Now Sheridan Community Hospital's ambulatory practices are partnering with M-CEITA to qualify and attest for the Meaningful Use program.</div>
 </div>
 </div>
 <div class="slide-wrapper slide-height">
 <div class="slide">
 <div class="slide-content"><b>Alcona Health Center</b>
-MCEITA has assisted them in the registration process helping them to be one of the first FQHC’s in the state to get fully registered for the Medicaid incentives. We have been working side by side to work out inefficiencies they have discovered and are currently setting the path for Meaningful Use to be reached this fall.</div>
+MCEITA has assisted them in the registration process helping them to be one of the first FQHC's in the state to get fully registered for the Medicaid incentives. We have been working side by side to work out inefficiencies they have discovered and are currently setting the path for Meaningful Use to be reached this fall.</div>
 </div>
 </div>
 <div class="slide-wrapper slide-height">
 <div class="slide">
 <div class="slide-content"><b>Health Specialists of Lenawee</b>
-Health Specialists of Lenawee is an FQHC serving Lenawee County. This well-led and technically savvy group decided to work with M-CEITA when they learned of the opportunity through LPHO. They were extremely interested in the Meaningful Use incentive program, and the M-CEITA team’s expertise in Meaningful Use criteria has led this practice to a strong pathway towards successful Medicare and Medicaid attestation for the 6 physicians in the group.</div>
+Health Specialists of Lenawee is an FQHC serving Lenawee County. This well-led and technically savvy group decided to work with M-CEITA when they learned of the opportunity through LPHO. They were extremely interested in the Meaningful Use incentive program, and the M-CEITA team's expertise in Meaningful Use criteria has led this practice to a strong pathway towards successful Medicare and Medicaid attestation for the 6 physicians in the group.</div>
 </div>
 </div>
 <div class="slide-wrapper slide-height">
