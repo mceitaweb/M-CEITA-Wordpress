a2adc1a8744d2bfc580556819ad6736d40415577
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 15:47:09 -0400

    modified home.php to use fullwidthcontent.php

--- wp-content/themes/blogolife/home.php
+++ wp-content/themes/blogolife/home.php
@@ -2,17 +2,6 @@
 /*
 Template Name: Home
 */
-get_header(); ?>
-<div class="entry-content"><?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wplook' ) ); ?></div>
-<?php get_footer(); ?>
-<style type='text/css'>
-#primary {
-	visibility: hidden;
-}
-#secondary {
-	display: none;
-}
-#main {
-	background: white;
-}
-</style>
+get_header();
+get_template_part('fullwidthcontent') ;
+get_footer(); ?>
