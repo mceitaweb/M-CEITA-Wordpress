8af5ee60151445066cf43851709e4c8153d2d8d6
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 15:16:31 -0400

    added home.php template

---
+++ wp-content/themes/blogolife/home.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Home
+*/
+get_header(); ?>
+<div id="main">
+Test
+</div>
+<?php get_footer(); ?>
+<style type='text/css'>
+#primary {
+	visibility: hidden;
+}
+#secondary {
+	display: none;
+}
+#main {
+	background: white;
+}
+</style>
