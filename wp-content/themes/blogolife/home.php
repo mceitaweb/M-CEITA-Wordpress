e97e77abb39b57480ea68880a96869e765deecf9
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 16:05:16 -0400

    added full width white background to home.php

--- wp-content/themes/blogolife/home.php
+++ wp-content/themes/blogolife/home.php
@@ -2,6 +2,11 @@
 /*
 Template Name: Home
 */
+<style type='text/css'>
+.right-sidebar #main {
+	background: #FFF;
+}
+</style>
 get_header();
 get_template_part('fullwidthcontent') ;
 get_footer(); ?>
