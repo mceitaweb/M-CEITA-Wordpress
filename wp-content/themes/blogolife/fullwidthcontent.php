a9b557d9110b79d19e3f2fc4ca2fb55a8b4353e0
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 16:42:20 -0400

    added second picture to home page

--- wp-content/themes/blogolife/fullwidthcontent.php
+++ wp-content/themes/blogolife/fullwidthcontent.php
@@ -10,8 +10,11 @@
 <header class="entry-header"><h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'wplook' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1></header>
 
 	<div class="entry-content">
-	<div class="img-container" style="width: 400px; float: left; display: block;"><img src="http://204.106.31.83/wp-content/uploads/2012/03/mceita_home_pic.jpg" alt="" title="mceita_home_pic" width="380" height="253" class="size-full wp-image-574" /></div>
-<div class="options_container" style="width: 400px; float: left; display: block;"><h3>M-CEITA is your trusted advisor for adoption and effective use of Electronic Health Records (EHR). Which statement best describes you?</h3>
+	<div class="img-container" style="width: 400px; float: left; display: block;">
+	<img src="http://204.106.31.83/wp-content/uploads/2012/03/mceita_home_pic.jpg" alt="" title="mceita_home_pic" width="380" height="253" class="size-full wp-image-574" />
+	<img src="http://204.106.31.83/wp-content/uploads/2012/03/mceita_home_pic2.jpg" alt="" title="mceita_home_pic2" width="380" height="253" class="alignleft size-full wp-image-599" />
+	</div>
+<div class="options_container" style="width: 400px; float: left; display: block;"><h2>M-CEITA is your trusted advisor for adoption and effective use of Electronic Health Records (EHR). Which statement best describes you?</h2>
 <h2 class="handle"><a style="float: left;" href="#">I'm a healthcare provider who has already adopted an EHR</a></h2>
 <div class="service_content_container">
 <div class="block">
