73edbe8557ee7d4e3fa29d2865c38fd7422d4298
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 16:06:17 -0400

    modified placement of style tag

--- wp-content/themes/blogolife/home.php
+++ wp-content/themes/blogolife/home.php
@@ -2,11 +2,12 @@
 /*
 Template Name: Home
 */
+
+get_header();
+get_template_part('fullwidthcontent') ;
+get_footer(); ?>
 <style type='text/css'>
 .right-sidebar #main {
 	background: #FFF;
 }
 </style>
-get_header();
-get_template_part('fullwidthcontent') ;
-get_footer(); ?>
