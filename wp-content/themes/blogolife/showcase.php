aa28a769d983b720fbbb35b8f3a9dea841e0e511
Benjamin Malley <benjamin.malley@gmail.com>
2012-03-12 17:14:59 -0400

    new format for showcase template

--- wp-content/themes/blogolife/showcase.php
+++ wp-content/themes/blogolife/showcase.php
@@ -2,17 +2,12 @@
 /*
 Template Name: Showcase
 */
+
 get_header();
-get_template_part('content', 'single' ) ;
+get_template_part('fullwidthcontent') ;
 get_footer(); ?>
 <style type='text/css'>
-.col2 {
-	width: 925px;
-}
-.right-sidebar #content {
-	width: 972px;
-}
-.entry-meta {
-	visibility: hidden;
+.right-sidebar #main {
+	background: #FFF;
 }
 </style>
---
+++ wp-content/themes/blogolife/showcasecontent.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The default template for displaying content
+ * @package wplook
+ * @subpackage BlogoLife
+ * @since BlogoLife 1.0
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<header class="entry-header"><h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'wplook' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1></header>
+
+	<div class="entry-content">
+
+	<div id="showcase" class="slide-height">
+<div id="slide-reel" class="slide-height">
+<div class="slide-wrapper slide-height">
+<div class="slide">
+<div class="slide-content"><b>Westside PO: Edwin DeLange, DO</b>
+Dr. DeLange is a solo practitioner, utilizing Athena Health’s complete product (EPM, EHR, billing company component) for the last year. He attended the Athena user group meeting in Boston in April and is feeling very confident post meeting that he is on the right track for both improving the quality of care for his patients and achieving efficiency in his practice. Dr. DeLange has completed a Current State Assessment as well as a Meaningful Use Audit and Gap Analysis and has only has two measures to work on — the patient summary document and the security evaluation. M-CEITA is providing guidance and recommendations in order for his practice to achieve these measures.</div>
+</div>
+</div>
+<div class="slide-wrapper slide-height">
+<div class="slide">
+<div class="slide-content"><img src="http://204.106.31.83/wp-content/uploads/2012/02/mackinaw_trail_pediatrics-150x150.jpg" alt="" title="mackinaw_trail_pediatrics" width="150" height="150" class="alignleft size-thumbnail wp-image-537" /><b>Mackinaw Trail Pediatrics</b>
+A rural pediatric practice was struggling with the Medicaid registration process and was unable to seek M-CEITA services due to financial constraints. When the group found out that services were available due to the CIPA/MAG partnership with M-CEITA, they were eager to lean on the guidance of M-CEITA to successfully register all of their providers in the Medicaid incentive program. The practice is grateful for the joint efforts that afforded this opportunity and is now on track to receive their first year reimbursement with their certified EHR product.</div>
+</div>
+</div>
+<div class="slide-wrapper slide-height">
+<div class="slide">
+<div class="slide-content"><img src="http://204.106.31.83/wp-content/uploads/2012/02/sheridan_community_hospital-150x150.jpg" alt="" title="sheridan_community_hospital" width="150" height="150" class="alignleft size-thumbnail wp-image-536" /><b>Sheridan Community Hospital</b>
+Although Sheridan Community Hospital’s ambulatory practices had originally decided not to pursue the EHR incentive program–more technology seemed too big of a hurdle–Sheridan’s leaders heard an M-CEITA presentation about how to pursue Meaningful Use through the modular approach. Now Sheridan Community Hospital’s ambulatory practices are partnering with M-CEITA to qualify and attest for the Meaningful Use program.</div>
+</div>
+</div>
+<div class="slide-wrapper slide-height">
+<div class="slide">
+<div class="slide-content"><b>Alcona Health Center</b>
+MCEITA has assisted them in the registration process helping them to be one of the first FQHC’s in the state to get fully registered for the Medicaid incentives. We have been working side by side to work out inefficiencies they have discovered and are currently setting the path for Meaningful Use to be reached this fall.</div>
+</div>
+</div>
+<div class="slide-wrapper slide-height">
+<div class="slide">
+<div class="slide-content"><b>Health Specialists of Lenawee</b>
+Health Specialists of Lenawee is an FQHC serving Lenawee County. This well-led and technically savvy group decided to work with M-CEITA when they learned of the opportunity through LPHO. They were extremely interested in the Meaningful Use incentive program, and the M-CEITA team’s expertise in Meaningful Use criteria has led this practice to a strong pathway towards successful Medicare and Medicaid attestation for the 6 physicians in the group.</div>
+</div>
+</div>
+<div class="slide-wrapper slide-height">
+<div class="slide">
+<div class="slide-content"><b>Bayview Family Practice &amp; Baraga County Medical Practice</b>
+Our engagement with Bayview Family Practice &amp; Baraga County Medical Practice began with a gap analysis of their current implementation of CPSI and Meaningful Use/PCMH EHR related standards. The practices were not on the ONC-ATCB certified version of CPSI, so the project plan for this practice included upgrading to the certified version and implementing the workflow changes necessary to incorporate the MU and PCMH EHR related measures. M-CEITA is currently working with the practice on implementing Cielo as their patient registry as part of our gap analysis identified several deficiencies in the patient registry functionality of CPSI.</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+	</div>
+	<div class="clear"></div>
+</article>	
